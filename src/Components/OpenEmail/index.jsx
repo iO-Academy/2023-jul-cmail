1ec52429a02594b4fe5f8ff110b8d5ee669c0fb0
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-29 16:51:32 +0100

    Started HTML

--- src/Components/OpenEmail/index.jsx
+++ src/Components/OpenEmail/index.jsx
@@ -2,7 +2,10 @@ const OpenEmail = () => {
 
     return (
         <>
-        
+          <h3>Someone's name</h3>
+          <p>someonesname@gmail.com</p>
+          <h2>Email subject</h2>
+          <p>uofbf4u4r 4uvhof4boif4fh f4 nohi2fhfio4hfo4i hoifhfoif4hif hoi4fhfoi4v</p>  
         </>
     )
 

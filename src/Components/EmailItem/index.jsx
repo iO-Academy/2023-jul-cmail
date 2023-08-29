e8783cbffc6846f78c846940241208f852bfaa2a
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-08-29 11:22:11 +0100

    put placeholder text into email item to assist with bootstrap css. added to index.css border-box and margin housekeeping

--- src/Components/EmailItem/index.jsx
+++ src/Components/EmailItem/index.jsx
@@ -1,13 +1,14 @@
-const EmailItem = ({name, }) => {
+const EmailItem = ({name, subject, body, date_created}) => {
     return (
         <>
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="card-link">Card link</a>
-                    <a href="#" className="card-link">Another link</a>
+            <div className="card row col-3">
+                <div className="card-body ">
+                    <div className="d-flex justify-content-between">
+                        <h5 className="card-title">{name}</h5>
+                        <p>{date_created}</p>
+                    </div>
+                    <h6 className="card-subtitle mb-2 text-body-secondary">{subject}</h6>
+                    <p className="card-text">{body}</p>
                 </div>
             </div>
         </>
--- src/Components/EmailList/index.jsx
+++ src/Components/EmailList/index.jsx
@@ -3,9 +3,12 @@ import EmailItem from "../EmailItem"
 const EmailList = () => {
     return (
         <>
-            <EmailItem/>
-            <EmailItem/>
-            <EmailItem/>
+            <EmailItem 
+            name="Joe Bloggs" 
+            subject="where are my keys?" 
+            body="Mate I am locked out...." 
+            date_created="29-07-2023"/>
+            
         </>
     )
 }

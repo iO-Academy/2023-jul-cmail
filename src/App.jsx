c547225fde300eb3b19b3b0bb5f9fc730a68dd15
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-29 15:21:07 +0100

    Deleted file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 
 import { useState } from 'react'
-
 import EmailList from './Components/EmailList'
 import Header from './Components/Header'
 import Inbox from './Components/Inbox'
-
 
 
 function App() {

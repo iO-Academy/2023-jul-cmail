97dad0337f6898c28d1ac1da9d1f9d0b267b7259
iO Academy <thejoshsilasbryan@gmail.com>
2023-08-30 17:00:55 +0100

    s3t2 make sent email

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './Components/Header'
 import OpenEmail from './Components/OpenEmail'
 import Inbox from './Components/Inbox'
 import { useState } from 'react'
+import NewEmail from './Components/NewEmail'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <div className="row h-100 g-0">
+      <NewEmail/>
       <Header inboxCounter={inboxCount}/>
       <Inbox emailId={emailId} setInboxCount={setInboxCount} setEmailId={setEmailId}/>
       {emailId && <OpenEmail emailId={emailId}/>}

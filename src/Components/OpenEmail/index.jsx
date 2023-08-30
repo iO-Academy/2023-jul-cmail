06411b7e515fbb59f82ecb7237a87048bd3f9754
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-30 10:24:40 +0100

    Ready for Peer Review, added border

--- src/Components/OpenEmail/index.jsx
+++ src/Components/OpenEmail/index.jsx
@@ -1,16 +1,19 @@
 const OpenEmail = () => {
 
+  // const [displayOpenEmail, setDisplayOpenEmail] = useState(false)
+  
     return (
         <div className="px-5 py-5 col-md-7 col-lg-8 d-sm-none d-md-block d-lg-block">
-            <div className="d-flex justify-content-between">
-              <h3>Sender's name</h3><span>Date goes here</span>
-            </div>
-              <p>someonesname@gmail.com</p>
-              <h2>Email subject</h2>
-              <p>Dear user, </p>
-              <p>Body of email goes here. blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>  
+            <div className="border border-start-0 border-end-0 border-top border-bottom py-5"> 
+              <div className="d-flex justify-content-between">
+                <h3>Sender's name</h3><span>Date goes here</span>
+              </div>
+                <p>someonesname@gmail.com</p>
+                <h2>Email subject</h2>
+                <p>Dear user, </p>
+                <p>Body of email goes here. blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p> 
+              </div> 
         </div>
     )
-
 }
 export default OpenEmail

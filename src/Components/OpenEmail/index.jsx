87900718e64644687c499fd850def4b702e486d9
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-30 10:29:49 +0100

    Added API help for future tasks

--- src/Components/OpenEmail/index.jsx
+++ src/Components/OpenEmail/index.jsx
@@ -6,12 +6,12 @@ const OpenEmail = () => {
         <div className="px-5 py-5 col-md-7 col-lg-8 d-sm-none d-md-block d-lg-block">
             <div className="border border-start-0 border-end-0 border-top border-bottom py-5"> 
               <div className="d-flex justify-content-between">
-                <h3>Sender's name</h3><span>Date goes here</span>
+                <h3>Sender's name ("name")</h3><span>"date_created" goes here</span>
               </div>
-                <p>someonesname@gmail.com</p>
-                <h2>Email subject</h2>
-                <p>Dear user, </p>
-                <p>Body of email goes here. blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p> 
+                <p>someonesname@gmail.com ("email")</p>
+                <h2>Email subject ("subject")</h2>
+                <p>Dear user, ("body")</p>
+                <p>Body of email goes here. blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ("body")</p> 
               </div> 
         </div>
     )

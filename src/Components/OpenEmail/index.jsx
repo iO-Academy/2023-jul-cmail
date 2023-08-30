c6b1db022196636da745fc7a3a5a18f60ccfe5b1
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-30 11:03:22 +0100

    Changes

--- src/Components/OpenEmail/index.jsx
+++ src/Components/OpenEmail/index.jsx
@@ -8,12 +8,12 @@ const OpenEmail = () => {
             <button className="border border-black text-center" type="button">Close</button>  
               <div className="border border-start-0 border-end-0 border-top border-bottom mt-3 py-5"> 
                 <div className="d-flex justify-content-between">
-                  <h3>Sender's name ({email.name})</h3><span>({email.date_created}) Date goes here</span>
+                  <h3>Sender's name ({"email.name"})</h3><span>({"email.date_created"}) Date goes here</span>
                 </div>
-                  <p>someonesname@gmail.com ({email.email})</p>
-                  <h2>Email subject ({email.subject})</h2>
-                  <p>Dear user, ({email.body})</p>
-                  <p>Body of email goes here. blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ({email.body})</p> 
+                  <p>someonesname@gmail.com ({"email.email"})</p>
+                  <h2>Email subject ({"email.subject"})</h2>
+                  <p>Dear user, ({"email.body"})</p>
+                  <p>Body of email goes here. blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ({"email.body"})</p> 
                 </div> 
           </div>
         </>

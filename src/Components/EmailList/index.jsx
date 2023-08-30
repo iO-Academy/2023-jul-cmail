0af8b5a949b39bc6a0652c5cc6958b6afc6e79f4
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-08-30 10:08:51 +0100

    ready for TR Pass1

--- src/Components/EmailList/index.jsx
+++ src/Components/EmailList/index.jsx
@@ -6,14 +6,14 @@ const EmailList = ({emails}) => {
     return (
         <div className="col-12 col-sm-12 col-md-4 col-lg-3 email-list">
             {emails.map(email => 
-                    <EmailPreview
-                        key={email.id} 
-                        name={email.name} 
-                        subject={email.subject} 
-                        body={email.body}
-                        date_created={email.date_created}
-                        read={email.read}
-                    />
+                <EmailPreview
+                    key={email.id} 
+                    name={email.name} 
+                    subject={email.subject} 
+                    body={email.body}
+                    date_created={email.date_created}
+                    read={email.read}
+                />
             )}
         </div>
     )

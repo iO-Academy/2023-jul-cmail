9275473ba1fdfa0b18736a5f0cbfef05106ada32
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-31 10:39:19 +0100

    Review changes and error handling

--- src/Components/OpenEmail/index.jsx
+++ src/Components/OpenEmail/index.jsx
@@ -5,6 +5,8 @@ import Button from "../Button"
 
 const OpenEmail = ({emailId, setRefreshEmails, closeEmail}) => {
     const [email, setEmail ] = useState(false)
+    const [formattedDate, setFormattedDate] = useState()
+    const [error, setError] = useState(false)
 
     const setEmailAsRead = async () => {
       await fetch(`http://localhost:8080/emails/${emailId}`, {
@@ -16,10 +18,9 @@ const OpenEmail = ({emailId, setRefreshEmails, closeEmail}) => {
         let response = await fetch(`http://localhost:8080/emails/${emailId}`)
         let email = await response.json()
         setEmail(email.data.email)
+        setError(false)
     }
 
-    const [formattedDate, setFormattedDate] = useState()
-    
     useEffect(() => {
         const formattedDate = formatDate(email.date_created)
         setFormattedDate(formattedDate)
@@ -31,22 +32,29 @@ const OpenEmail = ({emailId, setRefreshEmails, closeEmail}) => {
         .then(() => {
           setRefreshEmails(emailId)
         })
+        .catch(() => {
+          setError('Unexpected error occured!')
+        })
     }, [emailId])
     
     return (
-        <> 
-          <div className="p-md-5 pt-md-3 p-3 col-12 col-md-6 col-lg-8 openEmail">
+      <div className="p-md-5 pt-md-3 p-3 col-12 col-md-6 col-lg-8 openEmail">
+        {error === false ?
+          (<>
             <Button onClick={closeEmail} text={'Close'}/> 
-             <div className="border border-start-0 border-end-0 border-top border-bottom mt-3 py-5"> 
-                <div className="d-flex justify-content-between">
-                  <h3>{email.name}</h3><span>{formattedDate}</span>
-                </div>
-                  <p>{email.email}</p>
-                  <h2>{email.subject}</h2>
-                  <p>{email.body}</p>
-                </div> 
-          </div>
-        </>
+            <div className="border border-start-0 border-end-0 border-top border-bottom mt-3 py-5"> 
+              <div className="d-flex justify-content-between">
+                <h3>{email.name}</h3><span>{formattedDate}</span>
+              </div>
+                <p>{email.email}</p>
+                <h2>{email.subject}</h2>
+                <p>{email.body}</p>
+            </div> 
+          </>
+          )
+          :
+          (<h1 className="pt-5 text-center text-danger">{error}</h1>)}
+      </div>
     )
 }
 

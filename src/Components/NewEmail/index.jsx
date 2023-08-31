7b919ac095598178b223d0a0bd821c1b35247a28
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-08-31 09:42:27 +0100

    new email component is more responsive, still needs to only take up full screen on mobile though

--- src/Components/NewEmail/index.jsx
+++ src/Components/NewEmail/index.jsx
@@ -3,24 +3,24 @@ import "./NewEmail.css"
 const NewEmail = ({handleInput}) => {
 
     return (
-    <div className="newEmail w-50 bg-white position-fixed border"> 
-        <div className="mb-3">      
-            <label htmlFor="to" className="form-label"></label>
-            <input onInput={handleInput} type="email" className="form-control form-control-lg" placeholder="To"></input>
-        </div>
-        <div className="mb-3">
-            <label htmlFor="subject" className="form-label"></label>
-            <input className="form-control form-control-lg" placeholder="Subject"></input>
-        </div>
-        <div className="mb-3">
-            <label htmlFor="emailContent" className="form-label"></label>
-            <textarea className="form-control" rows="10"></textarea>
+        <div className="col-12 h-xs-100 col-md-7 offset-md-2 offset-lg-1 col-lg-5 newEmail bg-white position-fixed border"> 
+            <div className="mb-3">      
+                <label htmlFor="to" className="form-label"></label>
+                <input onInput={handleInput} type="email" className="form-control form-control-lg" placeholder="To"></input>
             </div>
-        <div className="mt-4 ms-2 d-flex justify-content-end">
-            <button type="button" className="btn btn-secondary">Cancel</button>
-            <button type="button" className="btn btn-success">Send</button>
+            <div className="mb-3">
+                <label htmlFor="subject" className="form-label"></label>
+                <input className="form-control form-control-lg" placeholder="Subject"></input>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="emailContent" className="form-label"></label>
+                <textarea className="form-control" rows="25"></textarea>
+                </div>
+            <div className="mt-4 ms-2 d-flex justify-content-end">
+                <button type="button" className="btn btn-secondary">Cancel</button>
+                <button type="button" className="btn btn-success">Send</button>
+            </div>
         </div>
-    </div>
     )
 }
 

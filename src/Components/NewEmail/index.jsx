f0cc794dda2f7912b59355c976fd3b475aff5ce8
iO Academy <thejoshsilasbryan@gmail.com>
2023-08-31 11:07:38 +0100

    m

--- src/Components/NewEmail/index.jsx
+++ src/Components/NewEmail/index.jsx
@@ -3,7 +3,7 @@ import "./NewEmail.css"
 const NewEmail = ({handleInput}) => {
 
     return (
-        <div className="col-12 h-xs-100 col-md-7 offset-md-2 offset-lg-1 col-lg-5 newEmail bg-white position-fixed border"> 
+        <div className="col-12 col-md-7 offset-md-2 offset-lg-1 col-lg-5 newEmail bg-white position-fixed border"> 
             <div className="mb-3">      
                 <label htmlFor="to" className="form-label"></label>
                 <input onInput={handleInput} type="email" className="form-control form-control-lg" placeholder="To"></input>
@@ -14,11 +14,11 @@ const NewEmail = ({handleInput}) => {
             </div>
             <div className="mb-3">
                 <label htmlFor="emailContent" className="form-label"></label>
-                <textarea className="form-control" rows="25"></textarea>
+                <textarea className="form-control" rows="10"></textarea>
                 </div>
             <div className="mt-4 ms-2 d-flex justify-content-end">
-                <button type="button" className="btn btn-secondary">Cancel</button>
-                <button type="button" className="btn btn-success">Send</button>
+                <button type="button" className="btn btn-secondary m-1">Cancel</button>
+                <button type="button" className="btn btn-success m-1">Send</button>
             </div>
         </div>
     )

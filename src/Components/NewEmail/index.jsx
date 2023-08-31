905592a13d76c632aad5a984a4f8e33c3f5bed15
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-31 14:00:45 +0100

    Ready for peer review - email sends

--- src/Components/NewEmail/index.jsx
+++ src/Components/NewEmail/index.jsx
@@ -1,47 +1,61 @@
-import { useEffect, useState } from "react"
+import {useState } from "react"
 import "./NewEmail.css"
 
-const NewEmail = ({cancelNewEmail}) => {
+const NewEmail = ({cancelNewEmail, handleInput}) => {
 
-    const [sendButton, setSendButton] = useState(false)
-    const [sentEmail, setSentEmail] = useState(false)
+    const [address, setAddress] = useState('')
+    const [subject, setSubject] = useState('')
+    const [body, setBody] = useState('')
+
+    const handleAddress = (e) => {
+        setAddress(e.target.value)
+    }
+
+    const handleSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+    const handleBody = (e) => {
+        setBody(e.target.value)
+    }
     
     const sendEmail = async () => {
-        const response = await fetch (`http://localhost:8080/emails/`, {
+        const sentEmail = {
+            email: address,
+            name: 'Alex is a loser',
+            subject: subject,
+            body: body
+        }
+        const response = await fetch (`http://localhost:8080/emails`, {
             method: 'POST',
-            body: JSON.stringify(data.email),
+            body: JSON.stringify(sentEmail),
             headers: {
                 'content-type': 'application/json'
             }
         })
-        let data = await response.json()
-        console.log(data)
+        let emailResponseData = await response.json()
+        if (emailResponseData.data.sent) {
+            cancelNewEmail()
+        } 
     }
-
-    useEffect(() => {
-        sendEmail()
-        .then()
-    })
-
-
 
     return (
         <div className="col-12 col-md-7 offset-md-2 offset-lg-1 col-lg-5 newEmail bg-white position-fixed border"> 
             <div className="mb-3">      
                 <label htmlFor="to" className="form-label"></label>
-                <input type="email" className="form-control form-control-lg" placeholder="To"></input>
+                <input onInput={handleAddress} type="email" className="form-control form-control-lg" placeholder="To"></input>
             </div>
             <div className="mb-3">
                 <label htmlFor="subject" className="form-label"></label>
-                <input className="form-control form-control-lg" placeholder="Subject" value={''}></input>
+                <input onInput={handleSubject} className="form-control form-control-lg" placeholder="Subject"></input>
             </div>
             <div className="mb-3">
                 <label htmlFor="emailContent" className="form-label"></label>
-                <textarea className="form-control" rows="10" value={''}></textarea>
+                <textarea onInput={handleBody} className="form-control" rows="10"></textarea>
                 </div>
             <div className="mt-4 ms-2 d-flex justify-content-end">
                 <button type="button" className="btn btn-secondary m-1" onClick={cancelNewEmail}>Cancel</button>
-                <button type="button" className="btn btn-success m-1">Send</button>
+                <button type="button" className="btn btn-success m-1" onClick={sendEmail}>Send</button>
             </div>
         </div>
     )
@@ -49,8 +63,3 @@ const NewEmail = ({cancelNewEmail}) => {
 
 export default NewEmail      
 
-// // const [cancelButton, setCancelButton] = useState(false)
-
-    // const handleSend = () => {
-    //     setSendButton(true)
-    // }
